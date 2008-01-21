2465f2b03e0649a17ba3cc9623f0a5a53faed785
Nathan Fredrickson <nathan@silverorange.com>
2008-01-21 19:55:55 +0000

    Update persistent data.
    svn commit r26218

---
+++ sql/persistent-data/adminuser.sql
@@ -0,0 +1,45 @@
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UTF8';
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Name: adminuser_id_seq; Type: SEQUENCE SET; Schema: public; Owner: php
+--
+
+SELECT pg_catalog.setval(pg_catalog.pg_get_serial_sequence('adminuser', 'id'), 14, true);
+
+
+--
+-- Data for Name: adminuser; Type: TABLE DATA; Schema: public; Owner: php
+--
+
+ALTER TABLE adminuser DISABLE TRIGGER ALL;
+
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (1, 'nick@silverorange.com', 'Nick Burka', 'f12d534540fdc2b0cd8389c4a54ad1fe', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (2, 'nathan@silverorange.com', 'Nathan', 'bfc4fd79ea7fdf1240db016e29ae417a', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (3, 'isaac@silverorange.com', 'Isaac', '64097f46b63fe377b590fa1ad00c8de2', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (4, 'dan@silverorange.com', 'Dan', '57f417b438b12da673e661e998e5bb92', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (5, 'desroches@silverorange.com', 'Stephen DesRoches', 'f1d5981e34c60eb678516587a27bdb53', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (6, 'mike@silverorange.com', 'Mike', '16590174c8d4afae49468e699987f99b', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (7, 'dave@silverorange.com', 'Dave', 'ae0ce887676b6d4fa6db438c94daca83', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (8, 'steven@silverorange.com', 'Steven', '57f417b438b12da673e661e998e5bb92', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (9, 'daniel@silverorange.com', 'Daniel', '6aecdfe8b004d1e8cb1e42c4414687a9', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (10, 'dennis@silverorange.com', 'Dennis', '57f417b438b12da673e661e998e5bb92', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (11, 'kelly@silverorange.com', 'Kelly', 'ff5c514afb898c582a88e6edc330c5dd', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (12, 'charleswaddell@gmail.com', 'Charles', '93d1ede729ffa6b33e5d00118e8c231a', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (13, 'keith@silverorange.com', 'Keith', '81bb7478bc9cd5814189b21b097a9787', '', NULL, false, true, NULL, true, NULL, NULL);
+INSERT INTO adminuser (id, email, name, "password", password_salt, password_tag, force_change_password, enabled, menu_state, "show", description, bodytext) VALUES (14, 'charlesp@silverorange.com', 'Charles P.', '33d19d605e33169ca0bf4b19ef8fad9b', '', NULL, false, true, NULL, true, NULL, NULL);
+
+
+ALTER TABLE adminuser ENABLE TRIGGER ALL;
+
+--
+-- PostgreSQL database dump complete
+--
+
